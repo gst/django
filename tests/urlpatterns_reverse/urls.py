864552d6ab2c7cb2963329d558d0ad680bd2d45a
Grégory Starck <gregory.starck@savoirfairelinux.com>
2016-01-05 16:47:48 -0500

    Add: a test data to show problem with nested partials views

--- tests/urlpatterns_reverse/urls.py
+++ tests/urlpatterns_reverse/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from tests.urlpatterns_reverse.views import empty_view_nested_partial
 
 from .views import (
     absolute_kwargs_view, defaults_view, empty_view, empty_view_partial,
@@ -62,6 +63,7 @@ urlpatterns = [
 
     # Partials should be fine.
     url(r'^partial/', empty_view_partial, name="partial"),
+    url(r'^partial_nested/', empty_view_nested_partial, name="partial_nested"),
     url(r'^partial_wrapped/', empty_view_wrapped, name="partial_wrapped"),
 
     # This is non-reversible, but we shouldn't blow up when parsing it.
